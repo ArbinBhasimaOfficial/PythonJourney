05cee997af6330f166f1a2667f837ddef0162e41
ArbinBhasima <arbinbhasima1@gmail.com>
2026-05-09 22:44:29 +0545

    Chapter 2 added : Boolean Operators

---
+++ Python_Programming_Basics/Chapter2/main.py
@@ -0,0 +1,42 @@
+# boolean data type
+
+spam = True
+print(spam) #True
+
+# error
+# spam1 = true
+# print(spam1)
+# Traceback (most recent call last):
+#   File "D:\ArbinBhasimaOfficial\PythonJourney\Python_Programming_Basics\Chapter2\main.py", line 7, in <module>
+#     spam1 = true
+#             ^^^^
+# NameError: name 'true' is not defined. Did you mean: 'True'?
+
+# True = 2 + 2
+#   File "<python-input-1>", line 1
+#     True = 2 + 2
+#     ^^^^
+# SyntaxError: cannot assign to True
+
+# Comparison Operators
+
+# spam1 = 2
+# spam2 = 2
+# spam3 = 4
+# name = 'loki'
+# name1 = 'thor'
+# name2 = 'thor'
+# print(bool(spam1 == spam2)) #True
+# print(bool(spam3 == spam2)) #False
+# print(bool(spam1 != spam2)) #False
+# print(bool(spam3 != spam2)) #True
+# print(bool(name1 == name2)) #True
+
+
+# spam = 42
+# trash = 123
+# eggCount = 231
+# print(bool(spam <= trash)) # True
+# print(bool(spam >= trash)) # False
+# print(bool(eggCount<=100)) # False
+# print(bool(eggCount>=100)) # True
